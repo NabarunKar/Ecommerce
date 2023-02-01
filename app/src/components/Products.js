f25ccfce3dade6c87498dea732638f98f0676397
Soumyadeep Datta <soumyadeepdatta34.alternative@gmail.com>
2023-02-02 00:17:05 +0530

    json test data

---
+++ app/src/components/Products.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function Products() {
+    useEffect(() => {
+        effect
+        return () => {
+            cleanup
+        };
+    }, []);
+  return (
+    <div>Products</div>
+  )
+}
+
+export default Products
--- server/server.js
+++ server/server.js
@@ -2,18 +2,18 @@ const express = require("express");
 const http = require("http");
 const app = express();
 const cors = require("cors");
+const fs = require("fs");
+require("dotenv").config();
+
+const products = require("./data/products.json");
 
 const server = http.createServer(app);
+const port = process.env.PORT || 5000;
 
-app.get("/products", (req, res) => {
-  res.json({ products: ["p1", "p2", "p3", "p4"] });
+app.get("/products", async (req, res, next) => {
+  res.json(products);
 });
 
-app.listen(5000, () => {
-  console.table(
-    {
-      port:5000,
-      date:Date()
-    }
-  );
+app.listen(port, () => {
+  console.info(`Server started on port ${port}`);
 });
