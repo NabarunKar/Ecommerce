6244b845b4b594ac6c1b037a7e56381c778c42f2
Soumyadeep Datta <soumyadeepdatta34.alternative@gmail.com>
2023-04-21 01:18:12 +0530

    add review

--- app/src/components/Product.js
+++ app/src/components/Product.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import { Link, useParams } from "react-router-dom";
 import UseFetch from "../hooks/useFetch";
 import { useCartContext } from "../contexts/CartContext";
+import Reviews from "./Reviews";
 
 function Product() {
   const { id } = useParams();
@@ -32,53 +33,58 @@ function Product() {
       {error && <div>{error}</div>}
       {isPending && <div>Loading...</div>}
       {data && (
-        <div>
-          {data.stock > 0 && !checkItemIfExistsInCart(data._id) && (
-            <>
-              <button
-                disabled={quantity == 1}
-                onClick={() => decreaseQuantity()}
-              >
-                -
-              </button>
-              {quantity}
-              <button
-                disabled={quantity == data.stock}
-                onClick={() => increaseQuantity(data.stock)}
-              >
-                +
-              </button>
-              <br />
+        <>
+          <div>
+            {data.stock > 0 && !checkItemIfExistsInCart(data._id) && (
+              <>
+                <button
+                  disabled={quantity == 1}
+                  onClick={() => decreaseQuantity()}
+                >
+                  -
+                </button>
+                {quantity}
+                <button
+                  disabled={quantity == data.stock}
+                  onClick={() => increaseQuantity(data.stock)}
+                >
+                  +
+                </button>
+                <br />
+                <button
+                  onClick={() => {
+                    addToCart(quantity, data);
+                  }}
+                >
+                  Add to cart
+                </button>
+              </>
+            )}
+            {checkItemIfExistsInCart(data._id) && (
               <button
                 onClick={() => {
-                  addToCart(quantity, data);
+                  removeFromCart(data._id);
                 }}
               >
-                Add to cart
+                Remove from cart
               </button>
-            </>
-          )}
-          {checkItemIfExistsInCart(data._id) && (
-            <button
-              onClick={() => {
-                removeFromCart(data._id);
-              }}
-            >
-              Remove from cart
-            </button>
-          )}
-          <br />
-          <img src={data.thumbnail} alt="" />
-          <h1>{data.title}</h1>
-          <h3>Price: ${data.price}</h3>
-          {data.stock > 0 ? <button>Buy</button> : <h2>Out of stock</h2>}
-          <p>{data.description}</p>
-          {data.images.map((e, id) => (
-            <li key={id}>
-              <img src={e} alt="" />
-            </li>
-          ))}
-        </div>
+            )}
+            <br />
+            <img src={data.thumbnail} alt="" />
+            <h1>{data.title}</h1>
+            <h3>Price: ${data.price}</h3>
+            {data.stock > 0 ? <button>Buy</button> : <h2>Out of stock</h2>}
+            <p>{data.description}</p>
+            {data.images.map((e, id) => (
+              <li key={id}>
+                <img src={e} alt="" />
+              </li>
+            ))}
+          </div>
+          <div>
+            <Reviews id={id} />
+          </div>
+        </>
       )}
     </div>
   );
--- app/src/components/Reviews.js
+++ app/src/components/Reviews.js
@@ -1,9 +1,95 @@
-import React from 'react'
+import React, { useState } from "react";
+import UseFetch from "../hooks/useFetch";
+import { useAuthContext } from "../hooks/useAuthContext";
+import { Link } from "react-router-dom";
 
-function Reviews() {
+function Reviews(props) {
+  const [data, isPending, error] = UseFetch(
+    `/api/products/reviews/${props.id}`
+  );
+
+  const [review, setReview] = useState({
+    productId: props.id,
+    rating: 2.5,
+    content: "",
+    userId: "",
+  });
+
+  const { user } = useAuthContext();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    if (user) {
+      review.userId = user._id;
+    }
+    await fetch("/api/products/reviews/", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(review),
+    });
+  };
+
   return (
-    <div>Reviews</div>
-  )
+    <div>
+      {user && (
+        <div>
+          <h2>Write a review</h2>
+          <form onSubmit={handleSubmit}>
+            <h2>Rating : {review.rating}</h2>
+            0
+            <input
+              type="range"
+              min="0"
+              max="5"
+              step="0.5"
+              value={review.rating}
+              onChange={(e) =>
+                setReview({ ...review, rating: Number(e.target.value) })
+              }
+            />
+            5
+            <textarea
+              name=""
+              id=""
+              cols="30"
+              rows="10"
+              value={review.content}
+              placeholder="Your thoughts"
+              onChange={(e) =>
+                setReview({ ...review, content: e.target.value })
+              }
+            ></textarea>
+            <button>Submit</button>
+          </form>
+        </div>
+      )}
+      {error && <div>{error}</div>}
+      {isPending && <div>Loading...</div>}
+      {data && (
+        <div>
+          <h1>Reviews ({data.length})</h1>
+          {data.map((r) => (
+            <li key={r._id}>
+              <div>
+                <b>
+                  {r.userName} ({r.userId}) [{r.rating}]
+                </b>
+                {user && r.userId === user._id && (
+                  <>
+                    <Link>Edit</Link> <Link>Delete</Link>
+                  </>
+                )}
+                <p>{r.content}</p>
+              </div>
+            </li>
+          ))}
+        </div>
+      )}
+    </div>
+  );
 }
 
-export default Reviews
+export default Reviews;
