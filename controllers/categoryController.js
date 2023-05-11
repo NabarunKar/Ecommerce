7dce9b77054131f40a232b3de9976a451be68416
Soumyadeep Datta <soumyadeepdatta34.alternative@gmail.com>
2023-05-11 22:56:26 +0530

    pre middleware for category schema

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -56,12 +56,13 @@ exports.updateCategoryById = async (req, res) => {
 // Delete a category by id
 exports.deleteCategoryById = async (req, res) => {
   try {
-    const category = await Category.findByIdAndDelete(req.params.id);
+    const category = await Category.findById(req.params.id);
     if (!category) {
       return res
         .status(404)
         .json({ success: false, message: "Category not found" });
     }
+    await category.deleteOne();
     res
       .status(200)
       .json({ success: true, message: "Category deleted successfully" });
--- models/Category.js
+++ models/Category.js
@@ -1,10 +1,28 @@
 const mongoose = require("mongoose");
 
 const categorySchema = new mongoose.Schema({
-  name: { type: String, required: true },
+  name: { type: String, required: true, unique: true },
   description: { type: String, required: true },
   thumbnail: { type: String, required: true },
 });
+
+// add pre middleware to category schema
+categorySchema.pre(
+  "deleteOne",
+  { document: true, query: false },
+  async function (next) {
+    try {
+      // remove the category from the categories array of each product that contains it
+      await mongoose
+        .model("Product")
+        .updateMany({}, { $pull: { categories: this._id } });
+
+      next();
+    } catch (err) {
+      next(err);
+    }
+  }
+);
 
 const Category = mongoose.model("Category", categorySchema);
 
