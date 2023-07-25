be4a98f056ced035658c865d89a4e1a6ee657047
Soumyadeep Datta <soumyadeepdatta34.alternative@gmail.com>
2023-07-25 12:44:15 -0700

    minor change

--- app/src/pages/Home.js
+++ app/src/pages/Home.js
@@ -30,34 +30,6 @@ function Home() {
     },
   ];
 
-  // const products = [
-  //   {
-  //     id: 1,
-  //     title: "Product 1",
-  //     image: "https://via.placeholder.com/200?text=Product+1",
-  //   },
-  //   {
-  //     id: 2,
-  //     title: "Product 2",
-  //     image: "https://via.placeholder.com/200?text=Product+2",
-  //   },
-  //   {
-  //     id: 3,
-  //     title: "Product 3",
-  //     image: "https://via.placeholder.com/200?text=Product+3",
-  //   },
-  //   {
-  //     id: 4,
-  //     title: "Product 4",
-  //     image: "https://via.placeholder.com/200?text=Product+4",
-  //   },
-  //   {
-  //     id: 5,
-  //     title: "Product 5",
-  //     image: "https://via.placeholder.com/200?text=Product+5",
-  //   },
-  // ];
-
   const { products } = useProductContext();
 
   return (
