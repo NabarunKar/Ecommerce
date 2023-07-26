1b1085bbd87762f58d813270899853832d747443
Soumyadeep Datta <soumyadeepdatta34.alternative@gmail.com>
2023-07-26 10:11:18 -0700

    url updated

--- app/src/contexts/ProductContext.js
+++ app/src/contexts/ProductContext.js
@@ -28,7 +28,7 @@ const ProductProvider = ({ children }) => {
   const getProducts = (url) => {
     dispatch({ type: "LOADING" });
 
-    fetch(url)
+    fetch(`${process.env.REACT_APP_PROXY}${url}`)
       .then((res) => {
         if (!res.ok) {
           // useReducer -> set error
